dd63ec4ad9d6567aaae071bd1fc2f9c8338f616d
irishev <irishev@naver.com>
2017-11-28 12:16:39 +0900

    data set mod

--- pytorch/test2.py
+++ pytorch/test2.py
@@ -29,7 +29,7 @@ def read_bson(bson_path, num_records, with_categories, global_offset):
     """
     offset = global_offset
     rows = {}
-    with open(bson_path, "rb") as f, tqdm(total=num_records) as pbar:
+    with open(bson_path, "rb") as f:
         f.seek(offset)
         records_read = 0
         while True:
@@ -56,8 +56,6 @@ def read_bson(bson_path, num_records, with_categories, global_offset):
             offset += length
             f.seek(offset)
             records_read += 1
-            pbar.update()
-    pbar.close()
     columns = ["num_imgs", "offset", "length"]
     if with_categories:
         columns += ["category_id"]
@@ -116,63 +114,101 @@ class CdiscountDataset(data_utils.Dataset):
     def __len__(self):
         return self.metadata.index.values.shape[0]
 
+
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                     stride=stride, padding=1)
+
+
+# Residual Block
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=2, pooling=2):
+        super(ResidualBlock, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1 + (pooling//3)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.MaxPool2d(pooling, stride=stride),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2),
+            nn.BatchNorm2d(out_channels),
+        )
+
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out += self.layer2(x)
+        return out
+
+
+class ResidualBlock2(nn.Module):
+    def __init__(self, channels):
+        super(ResidualBlock2, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(channels),
+        )
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out += x
+        return out
+
+
 # Neural Network Model (1 hidden layer)
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 8, kernel_size=3, padding=1),
-            nn.BatchNorm2d(8),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
-            nn.Conv2d(8, 16, kernel_size=3, padding=1),
-            nn.BatchNorm2d(16),
-            nn.MaxPool2d(2),
-            nn.ReLU(),)
-
-        self.layer2_1 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, padding=2),
-            # nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.Conv2d(32, 128, kernel_size=4, stride=4),
-            nn.ReLU())
-
-        self.layer2_2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, padding=2),
-            # nn.BatchNorm2d(16),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(inplace=True),
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            # nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=2, stride=2),
-            nn.ReLU())
-
-        self.layer2_3 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, padding=2),
-            # nn.BatchNorm2d(16),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            # nn.BatchNorm2d(16),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            # nn.BatchNorm2d(16),
-            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, stride=2),
         )
 
-        self.layer3 = nn.Sequential(
-            nn.Linear(23*23*128, 512),
-            nn.BatchNorm1d(512),
+        self.layer2 = self.residual(64, 192, 3)
+
+        self.layer3 = self.residual2(192)
+
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(192, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.Linear(512, 5270))
+            nn.AvgPool2d(22)
+        )
 
         self.fc = nn.Sequential(
-            nn.Linear(45 * 45 * 16, 1024),
-            nn.BatchNorm1d(1024),
-            nn.ReLU(),
-            nn.Linear(1024, 5270))
+            nn.Dropout(inplace=True),
+            nn.Linear(512, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 5270),)
+
+    def residual(self, ind, outd, pooling):
+        layers = []
+        layers.append(ResidualBlock(ind, outd, pooling=pooling))
+        return nn.Sequential(*layers)
+
+    def residual2(self, d):
+        layers = []
+        layers.append(ResidualBlock2(d))
+        return nn.Sequential(*layers)
 
     def forward(self, x):
         # h0 = Variable(torch.randn(2, x.size(0), 300).cuda())
@@ -190,10 +226,14 @@ class Net(nn.Module):
         out = self.layer3(out)'''
 
         out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer5(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
 
         return out
+
 
 net = Net()
 net.cuda()
@@ -202,7 +242,7 @@ learning_rate=0.001
 
 # Loss and Optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(net.parameters(), weight_decay=1e-5)
 
 if __name__ == "__main__":
     TRAIN_BSON_FILE = 'C:/test/train.bson'
@@ -210,47 +250,52 @@ if __name__ == "__main__":
     CATEGS = 'D:/Download/category_names.csv'
     # Chenge this weh running on your machine
     N_TRAIN = 7069896
-    R_TRAIN = 65536
-    R_TEST = 1024
-    BS = 128
+    R_TRAIN = 64
+    R_TEST = 1
+    BS = 64
     N_THREADS = 1
-    EPOCH = 20
+    EPOCH = 100
 
     # mapping the catigores into 0-5269 range
     cat2idx, idx2cat = make_category_tables(CATEGS)
-    # Scanning the metadata
-    meta_data, go = read_bson(TRAIN_BSON_FILE, R_TRAIN, with_categories=True, global_offset=0)
-    meta_data.category_id = np.array([cat2idx[ind] for ind in meta_data.category_id])
 
-    meta_data2, _ = read_bson(TRAIN_BSON_FILE, R_TEST, with_categories=True, global_offset=go)
-    meta_data2.category_id = np.array([cat2idx[ind] for ind in meta_data2.category_id])
-
-    # meta_data = meta_data.iloc[np.arange(500)]  # Remove this!!!
     # Dataset and loader
-    train_dataset = CdiscountDataset(TRAIN_BSON_FILE, meta_data, transf.ToTensor())
-    loader = data_utils.DataLoader(train_dataset, batch_size=BS, num_workers=0, shuffle=True)
+    loader = []
+    test_loader = []
+    go=0
 
-    test_dataset = CdiscountDataset(TRAIN_BSON_FILE, meta_data2, transf.ToTensor())
-    test_loader = data_utils.DataLoader(test_dataset, batch_size=BS, num_workers=0, shuffle=False)
-    # Let's go fetch some data!
+    for i in range(0, 1024):
+        # Scanning the metadata
+        meta_data, go = read_bson(TRAIN_BSON_FILE, R_TRAIN, with_categories=True, global_offset=go)
+        meta_data.category_id = np.array([cat2idx[ind] for ind in meta_data.category_id])
+
+        meta_data2, _ = read_bson(TRAIN_BSON_FILE, R_TEST, with_categories=True, global_offset=go)
+        meta_data2.category_id = np.array([cat2idx[ind] for ind in meta_data2.category_id])
+
+        # meta_data = meta_data.iloc[np.arange(500)]  # Remove this!!!
+
+        train_dataset = CdiscountDataset(TRAIN_BSON_FILE, meta_data, transf.ToTensor())
+        loader += [data_utils.DataLoader(train_dataset, batch_size=BS, num_workers=0, shuffle=True)]
+
+        test_dataset = CdiscountDataset(TRAIN_BSON_FILE, meta_data2, transf.ToTensor())
+        test_loader += [data_utils.DataLoader(test_dataset, batch_size=1, num_workers=0, shuffle=False)]
+        # Let's go fetch some data!
 
     for epoch in range(EPOCH):
-        for i, (batch, target) in enumerate(loader):
-            # Convert torch tensor to Variable
-            images = Variable(batch.cuda())
-            labels = Variable(target.cuda())
+        pbar = tqdm(total=len(loader))
+        for loaderf in loader:
+            for i, (batch, target) in enumerate(loaderf):
+                # Convert torch tensor to Variable
+                images = Variable(batch.cuda())
+                labels = Variable(target.cuda())
 
-            # Forward + Backward + Optimize
-            optimizer.zero_grad()  # zero the gradient buffer
-            outputs = net(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+                # Forward + Backward + Optimize
+                optimizer.zero_grad()  # zero the gradient buffer
+                outputs = net(images)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
 
-            if i == 0:
-                print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
-                          % (epoch + 1, EPOCH, i + 1, (len(train_dataset) // BS) + 1, loss.data[0]))
-                pbar = tqdm(total=len(loader))
             pbar.update()
         pbar.close()
 
@@ -258,14 +303,15 @@ if __name__ == "__main__":
         net.eval()
         correct = 0
         total = 0
-        for batch, target in test_loader:
-            images = Variable(batch).cuda()
-            outputs = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += target.size(0)
-            correct += (predicted.cpu() == target).sum()
+        for test_loaderf in test_loader:
+            for batch, target in test_loaderf:
+                images = Variable(batch).cuda()
+                outputs = net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += target.size(0)
+                correct += (predicted.cpu() == target).sum()
         net.train()
-        print('Accuracy of the network on the %d test images: %f %%' % (R_TEST, (100.0 * correct / total)))
+        print('%dth accuracy of the network on the %d test images: %f %%' % (epoch, R_TEST, (100.0 * correct / total)))
 
     # Save the Model
     torch.save(net.state_dict(), 'model.pkl')
